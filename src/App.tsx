8d48096b72daf5c8e1b75c298c3488b12d4480d0
Carl Keneth Baseleres <carl.baseleres@essilor.com>
2023-11-18 12:07:05 +0800

    remove unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { jsPDF } from "jspdf";
 import { generatePDF } from "./helpers/generate_pdf";
 
 function App() {
